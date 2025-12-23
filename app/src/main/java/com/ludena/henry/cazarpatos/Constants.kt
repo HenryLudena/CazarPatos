187c4eadebdae7e3b40946fac797ce79b34f9d58
HenryLudenaPyX <ludenahenry2@gmail.com>
2025-12-22 23:47:42 -0500

    Agregar formas de guardado (external, internal, cifrado, XML)

--- app/src/main/java/com/ludena/henry/cazarpatos/Constants.kt
+++ app/src/main/java/com/ludena/henry/cazarpatos/Constants.kt
@@ -1,3 +1,6 @@
 package com.ludena.henry.cazarpatos
-
+const val MIN_PASSWORD_LENGTH = 8
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
 const val EXTRA_LOGIN = "EXTRA_LOGIN"
+const val SHAREDINFO_FILENAME = "mySharedInformation.dat"
--- app/src/main/java/com/ludena/henry/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/ludena/henry/cazarpatos/LoginActivity.kt
@@ -4,33 +4,38 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var manejadorArchivo: FileHandler
     lateinit var editTextEmail: EditText
     lateinit var editTextPassword:EditText
     lateinit var buttonLogin: Button
     lateinit var buttonNewUser:Button
+    lateinit var checkBoxRecordarme: CheckBox
     lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
-{ v, insets ->
-            val systemBars =
-insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
-systemBars.bottom)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
         //Inicialización de variables
+        manejadorArchivo = SharedPreferencesManager(this)
         editTextEmail = findViewById(R.id.editTextEmail)
         editTextPassword = findViewById(R.id.editTextPassword)
         buttonLogin = findViewById(R.id.buttonLogin)
         buttonNewUser = findViewById(R.id.buttonNewUser)
+        checkBoxRecordarme = findViewById(R.id.checkBoxRecordarme)
+
+        LeerDatosDePreferencias()
+
         //Eventos clic
         buttonLogin.setOnClickListener {
             val email = editTextEmail.text.toString()
@@ -38,6 +43,8 @@ systemBars.bottom)
             //Validaciones de datos requeridos y formatos
             if(!validateRequiredData())
                 return@setOnClickListener
+            //Guardar datos en preferencias.
+            GuardarDatosEnPreferencias()
             //Si pasa validación de datos requeridos, ir a pantalla principal
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra(EXTRA_LOGIN, email)
@@ -50,6 +57,40 @@ systemBars.bottom)
         mediaPlayer=MediaPlayer.create(this, R.raw.title_screen)
         mediaPlayer.start()
     }
+
+    private fun LeerDatosDePreferencias(){
+        val listadoLeido = manejadorArchivo.ReadInformation()
+        if(listadoLeido.first != null){
+            checkBoxRecordarme.isChecked = true
+        }
+        editTextEmail.setText ( listadoLeido.first )
+        editTextPassword.setText ( listadoLeido.second )
+    }
+
+    private fun GuardarDatosEnPreferencias(){
+        val email = editTextEmail.text.toString()
+        val clave = editTextPassword.text.toString()
+        val listadoAGrabar:Pair<String,String>
+        if(checkBoxRecordarme.isChecked){
+            listadoAGrabar = email to clave
+        }
+        else{
+            listadoAGrabar ="" to ""
+        }
+        //Guarda información en SharedPreferences en texto plano
+        manejadorArchivo.SaveInformation(listadoAGrabar)
+        //Guardar la información encriptada
+        manejadorArchivo = EncriptedSharedPreferencesManager(this)
+        manejadorArchivo.SaveInformation(listadoAGrabar )
+        //Guardar la información en archivo interno
+        manejadorArchivo = FileInternalManager(this)
+        manejadorArchivo.SaveInformation(listadoAGrabar )
+        //Guardar la información en archivo externo
+        manejadorArchivo = FileExternalManager(this)
+        manejadorArchivo.SaveInformation(listadoAGrabar )
+
+    }
+
     private fun validateRequiredData():Boolean{
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
@@ -59,13 +100,11 @@ systemBars.bottom)
             return false
         }
         if (password.isEmpty()) {
-
             editTextPassword.setError(getString(R.string.error_password_required))
             editTextPassword.requestFocus()
             return false
         }
         if (password.length < 3) {
-
             editTextPassword.setError(getString(R.string.error_password_min_length))
             editTextPassword.requestFocus()
             return false
